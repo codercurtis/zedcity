d5ac197797f41f98fb53d29076dd81bb41e2fc0c
codercurtis <codercurtis@gmail.com>
2016-01-14 00:58:41 -0700

    actual stuff I intended to commit initially

---
+++ Assets/Scripts/BuildingScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingScript : MonoBehaviour {
+
+	public GameObject building;
+	private GameObject _building;
+	
+	void Start () {
+		building = Master.instance.bTypes[RandomBuilding()]; 
+		_building = GameObject.Instantiate(building,new Vector3(transform.position.x,building.transform.localScale.y/2f,transform.position.z),Quaternion.identity) as GameObject;
+		_building.transform.parent = transform;
+	}
+	
+	int RandomBuilding(){
+		return Random.Range(0,3);
+	}
+}
---
+++ Assets/Scripts/Master.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Master : MonoBehaviour {
+
+	private static Master s_instance = null;
+	public int gridSize = 5;
+	public GameObject[] bTypes;
+	
+	public GameObject rootBuildingObj;
+
+
+	//http://wiki.unity3d.com/index.php/AManagerClass
+	public static Master instance {
+		get{
+			if(s_instance == null){
+				s_instance = FindObjectOfType(typeof(Master)) as Master;
+			}
+			
+			if(s_instance == null){
+				GameObject obj = new GameObject("Master");
+				s_instance = obj.AddComponent(typeof(Master)) as Master;
+				Debug.Log("Manager created");
+			}
+			
+			return s_instance;
+		}
+	}
+		
+	void OnApplicationQuit() {
+        s_instance = null;
+    }
+	
+	void Awake(){
+		 DontDestroyOnLoad(instance);
+	}
+	
+	void Start(){
+		for(var i=0;i<gridSize;i++){
+			for(var j=0;j<gridSize;j++){
+				GameObject.Instantiate(rootBuildingObj,new Vector3((i+1)*25,0,(j+1)*25),Quaternion.identity);
+			}
+		}
+	}
+}
