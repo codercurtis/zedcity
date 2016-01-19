3fc767b6e8bb231615255e6f43d02133970bb349
codercurtis <codercurtis@gmail.com>
2016-01-18 18:13:47 -0700

    Text labels

--- Assets/Scripts/BuildingScript.cs
+++ Assets/Scripts/BuildingScript.cs
@@ -9,6 +9,8 @@ public class BuildingScript : MonoBehaviour {
 	public GameObject building;
 	public enum BuildingMode {hq = 0,zombie = 1,neutral = 2};
 	public BuildingMode _mode;
+	public GameObject labelHolder;
+
 	
 	public BuildingMode mode
 	{
@@ -39,16 +41,25 @@ public class BuildingScript : MonoBehaviour {
 		}
 	}
 	
-	void Start () {
+	void Start () { 
+	
 		GameObject buildingType = RandomBuilding();
 		building = GameObject.Instantiate(buildingType,new Vector3(transform.position.x,buildingType.transform.localScale.y/2f,transform.position.z),Quaternion.identity) as GameObject;
 		building.transform.parent = transform;
 		BoxCollider bc = gameObject.AddComponent<BoxCollider>() as BoxCollider;
 		bc.size = building.transform.localScale;
 		bc.center = new Vector3(0,buildingType.transform.localScale.y/2f,0);
+		
 		//The building may not have been ready on initial assignment of mode
 		this.mode = mode;
-	}
+		
+		//Create label objects
+		var temp = new GameObject();
+		labelHolder = GameObject.Instantiate(temp,new Vector3(transform.position.x,building.transform.localScale.y,transform.position.z),Camera.main.transform.rotation) as GameObject;
+		TextMesh mesh = labelHolder.AddComponent<TextMesh>();
+		mesh.anchor = TextAnchor.MiddleCenter;
+		mesh.text = "Hello World";
+	} 
 	
 	GameObject RandomBuilding(){
 		return Master.instance.bTypes[UnityEngine.Random.Range(0,Master.instance.bTypes.Length-1)];
---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Reflection;
+
+public class ButtonScript : MonoBehaviour {
+
+	bool active;
+	bool visible;
+	
+	public GameObject buttonObj;
+	
+	// Use this for initialization
+	void Start () {
+		buttonObj = new GameObject();
+		buttonObj.name = "Button Object";
+		buttonObj.transform.parent = transform;
+		buttonObj.transform.localPosition = new Vector3(0,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		buttonObj.transform.rotation = Camera.main.transform.rotation;
+	}
+}
