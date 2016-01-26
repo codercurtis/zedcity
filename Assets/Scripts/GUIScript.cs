90480f0684591c190e1477b9f0a4d058086f0b06
codercurtis <codercurtis@gmail.com>
2016-01-26 00:42:50 -0700

    Added GUIScript

---
+++ Assets/Scripts/GUIScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GUIScript : MonoBehaviour {
+	
+	float buttonWidth = 100;
+	float buttonHeight = 30;
+	void OnGUI(){
+		if(GUI.Button(new Rect(Screen.width/2-buttonWidth/2,Screen.height *0.01f,buttonWidth,buttonHeight),"Units")){
+		
+		}
+	}
+}
